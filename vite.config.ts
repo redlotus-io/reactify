dabe867b1163142f1027e86d528171f0076e3da2
tomimarkus991 <tomimarkusalber@gmail.com>
2022-04-20 20:51:19 +0300

    More setup

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 import tsconfigPaths from "vite-tsconfig-paths";
+import eslintPlugin from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({}), tsconfigPaths()],
+  plugins: [react(), VitePWA(), tsconfigPaths(), eslintPlugin()],
   publicDir: "assets",
 });
