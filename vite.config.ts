5439278ef689f43244c25cfd00363370d8456bf8
tomimarkus991 <tomimarkusalber@gmail.com>
2022-04-21 09:27:09 +0300

    Remove eslint plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,12 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 import tsconfigPaths from "vite-tsconfig-paths";
-import eslintPlugin from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA(),
-    tsconfigPaths(),
-    eslintPlugin({ include: "src/**/*.{js,jsx,ts,tsx}" }),
-  ],
+  plugins: [react(), VitePWA(), tsconfigPaths()],
+
   publicDir: "assets",
 });
-// ./src/**/*.{js,jsx,ts,tsx}
