0a85184b1716722d4122101134cda44579490b37
tomimarkus991 <tomimarkusalber@gmail.com>
2022-04-26 11:08:55 +0300

    Add PWA v1

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # React-Boilerplate
+
 Boilerplate for react
+
+change vite config names
+index.html names
+icons in public
--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,37 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 
 export default defineConfig({
-  plugins: [react(), VitePWA(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    VitePWA({
+      includeAssets: ["favicon.svg", "favicon.ico", "robots.txt", "apple-touch-icon.png"],
+      manifest: {
+        name: "React Boilerplate",
+        short_name: "RB",
+        description: "Boilerplate to get started fast",
+        theme_color: "#282c31",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     host: true,
   },
