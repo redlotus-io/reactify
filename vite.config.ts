8c4305f5a0f9ab58366d47cf13c135c023940859
tomimarkus991 <tomimarkusalber@gmail.com>
2022-04-26 20:51:19 +0300

    Add more icons

--- vite.config.ts
+++ vite.config.ts
@@ -39,20 +39,17 @@ export default defineConfig({
         orientation: "portrait",
         icons: [
           {
-            // src: "/icons/pwa-192x192.png",
-            src: "icons/favicon.svg",
+            src: "/icons/android-chrome-192x192",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            // src: "/icons/pwa-512x512.png",
-            src: "/icons/favicon.svg",
+            src: "/icons/android-chrome-512x512",
             sizes: "512x512",
             type: "image/png",
           },
           {
-            // src: "/icons/pwa-512x512.png",
-            src: "icons/favicon.svg",
+            src: "/icons/android-chrome-512x512",
             sizes: "512x512",
             type: "image/png",
             purpose: "any maskable",
