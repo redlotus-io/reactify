dfeba03720afb1cd941c09eb220a4e8181f8bf60
tomimarkus991 <tomimarkusalber@gmail.com>
2022-04-30 09:12:28 +0300

    Add different PWA after this commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@
 
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import checker from "vite-plugin-checker";
 import { VitePWA } from "vite-plugin-pwa";
 import tsconfigPaths from "vite-tsconfig-paths";
 
@@ -11,6 +12,7 @@ export default defineConfig({
   plugins: [
     react(),
     tsconfigPaths(),
+    checker({ typescript: true }),
     VitePWA({
       // mode: "development",
       srcDir: "src",
