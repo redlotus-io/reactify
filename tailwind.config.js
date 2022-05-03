de080b45e5d0efb615a9dcf3948f15da917f3f3e
tomimarkus991 <tomimarkusalber@gmail.com>
2022-05-03 17:36:14 +0300

    Add border gradient plugin and examples

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./src/*.{js,jsx,ts,tsx}"],
   darkMode: "class",
@@ -14,6 +17,19 @@ module.exports = {
       varela: ["Varela Round", "sans-serif"],
       sans: ["Rubik", "sans-serif"],
     },
+    linearBorderGradients: () => ({
+      colors: {
+        "light-green": [colors.emerald[500], colors.green[500], colors.lime[500]],
+        "light-blue": [colors.teal[500], colors.emerald[500], colors.green[500]],
+      },
+      background: {
+        white: "#fff",
+      },
+    }),
   },
-  plugins: [require("tailwind-scrollbar"), require("tailwind-scrollbar-hide")],
+  plugins: [
+    require("tailwind-scrollbar"),
+    require("tailwind-scrollbar-hide"),
+    require("tailwindcss-border-gradient-radius"),
+  ],
 };
